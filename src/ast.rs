56d2ea3de1666c898f9829e22c3e0db4a74d3119
Milo Moisson <milo@wiro.world>
2026-03-18 11:29:59 +0000

    add ast visitor for collection and resolution

--- src/ast.rs
+++ src/ast.rs
@@ -278,7 +278,6 @@ pub struct Block {
 pub struct FnDecl {
 	pub params: Vec<Param>,
 	pub ret: Option<Ty>,
-
 	pub span: Span,
 }
 
@@ -286,6 +285,7 @@ pub struct FnDecl {
 pub struct Param {
 	pub name: Ident,
 	pub ty: Ty,
+	pub id: NodeId,
 }
 
 #[derive(Debug, Clone)]
@@ -302,7 +302,8 @@ pub enum TyKind {
 	/// `* <ty>`
 	Pointer(Box<Ty>),
 	/// `& <ty>`
-	Reference(Box<Ty>),
+	// TODO: make references
+	// Reference(Box<Ty>),
 
 	// TODO: replace with tuple definition
 	Unit,
@@ -318,7 +319,13 @@ pub struct Path {
 #[derive(Debug, Clone)]
 pub struct PathSegment {
 	pub name: Ident,
-	pub generics: Vec<Ty>,
+	pub generics: GenericParams,
+	pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub struct GenericParams {
+	pub params: Vec<Ty>,
 	pub span: Span,
 }
 
@@ -331,11 +338,9 @@ impl Path {
 
 	pub(crate) fn is_match(&self, path: &[Symbol]) -> bool {
 		self.segments.len() == path.len()
-			&& self
-				.segments
-				.iter()
-				.zip(path)
-				.all(|(segment, sym)| segment.name.sym == *sym && segment.generics.is_empty())
+			&& self.segments.iter().zip(path).all(|(segment, sym)| {
+				segment.name.sym == *sym && segment.generics.params.is_empty()
+			})
 	}
 }
 
@@ -345,6 +350,20 @@ pub struct Item {
 	pub kind: ItemKind,
 	pub span: Span,
 	pub id: NodeId,
+}
+
+impl Item {
+	pub fn name(&self) -> Option<Ident> {
+		match self.kind {
+			ItemKind::Function(Function { name, .. })
+			| ItemKind::TypeAlias(TypeAlias { name, .. })
+			| ItemKind::Struct { name, .. }
+			| ItemKind::Enum { name, .. }
+			| ItemKind::Trait { name, .. } => Some(name),
+
+			ItemKind::TraitImpl { .. } | ItemKind::ForeignMod { .. } => None,
+		}
+	}
 }
 
 /// `type <name> [ = <ty> ] ;`
@@ -363,7 +382,16 @@ pub struct Function {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Generics(pub Vec<Ident>);
+pub struct Generics {
+	pub idents: Vec<Generic>,
+	pub span: Span,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Generic {
+	pub name: Ident,
+	pub id: NodeId,
+}
 
 #[derive(Debug)]
 pub enum ItemKind {
@@ -455,22 +483,406 @@ pub enum StmtKind {
 	Empty,
 }
 
-pub trait Visitor {
-	fn visit_root(&mut self, root: &Root);
+pub mod visit {
+	use super::*;
 
-	fn visit_attrs(&mut self, attrs: &[Attr]) {
-		for attr in attrs {
-			self.visit_attr(attr);
+	pub trait Visitor: Sized {
+		fn visit_root(&mut self, root: &Root) {
+			visit_root(self, root);
+		}
+
+		fn visit_attr(&mut self, attr: &Attr) {
+			visit_attr(self, attr);
+		}
+
+		fn visit_item(&mut self, item: &Item) {
+			visit_item(self, item);
+		}
+
+		fn visit_expr(&mut self, expr: &Expr) {
+			visit_expr(self, expr);
+		}
+
+		fn visit_block(&mut self, block: &Block) {
+			visit_block(self, block);
+		}
+
+		fn visit_stmt(&mut self, stmt: &Stmt) {
+			visit_stmt(self, stmt);
+		}
+
+		fn visit_ty(&mut self, ty: &Ty) {
+			visit_ty(self, ty);
+		}
+
+		fn visit_path(&mut self, path: &Path) {
+			visit_path(self, path);
+		}
+
+		fn visit_path_segment(&mut self, segment: &PathSegment) {
+			visit_path_segment(self, segment);
+		}
+
+		fn visit_generic_params(&mut self, params: &GenericParams) {
+			visit_generic_params(self, params);
+		}
+
+		fn visit_param(&mut self, param: &Param) {
+			visit_param(self, param);
+		}
+
+		fn visit_field_def(&mut self, field: &FieldDef) {
+			visit_field_def(self, field);
+		}
+
+		fn visit_variant(&mut self, variant: &Variant) {
+			visit_variant(self, variant);
+		}
+
+		fn visit_generics(&mut self, generics: &Generics) {
+			visit_generics(self, generics);
+		}
+
+		fn visit_ident(&mut self, ident: &Ident) {
+			visit_ident(self, ident);
+		}
+
+		fn visit_items(&mut self, items: &[Item]) {
+			for item in items {
+				self.visit_item(item);
+			}
+		}
+
+		fn visit_stmts(&mut self, stmts: &[Stmt]) {
+			for stmt in stmts {
+				self.visit_stmt(stmt);
+			}
+		}
+
+		fn visit_attrs(&mut self, attrs: &[Attr]) {
+			for attr in attrs {
+				self.visit_attr(attr);
+			}
+		}
+
+		fn visit_variants(&mut self, variants: &[Variant]) {
+			for variant in variants {
+				self.visit_variant(variant);
+			}
+		}
+
+		fn visit_fields(&mut self, fields: &[FieldDef]) {
+			for field in fields {
+				self.visit_field_def(field);
+			}
 		}
 	}
 
-	fn visit_attr(&mut self, attrs: &Attr);
+	pub fn visit_root<V: Visitor>(v: &mut V, Root { attrs, items }: &Root) {
+		v.visit_attrs(attrs);
+		v.visit_items(items);
+	}
 
-	fn visit_items(&mut self, items: &[Item]) {
-		for item in items {
-			self.visit_item(item);
+	pub fn visit_attr<V: Visitor>(
+		v: &mut V,
+		Attr {
+			path,
+			meta,
+			span: _,
+			id: _,
+		}: &Attr,
+	) {
+		v.visit_path(path);
+		match meta {
+			AttrMeta::None => {}
+			AttrMeta::Tuple(exprs) | AttrMeta::Map(exprs) | AttrMeta::List(exprs) => {
+				for expr in exprs {
+					v.visit_expr(expr);
+				}
+			}
 		}
 	}
 
-	fn visit_item(&mut self, item: &Item);
+	pub fn visit_item<V: Visitor>(
+		v: &mut V,
+		Item {
+			attrs,
+			kind,
+			span: _,
+			id: _,
+		}: &Item,
+	) {
+		v.visit_attrs(attrs);
+		match kind {
+			ItemKind::Function(Function { name, decl, body }) => {
+				let FnDecl {
+					params,
+					ret,
+					span: _,
+				} = decl;
+				v.visit_ident(name);
+				for param in params {
+					v.visit_param(param);
+				}
+				if let Some(ret) = ret {
+					v.visit_ty(ret);
+				}
+				if let Some(body) = body {
+					v.visit_block(body);
+				}
+			}
+			ItemKind::TypeAlias(TypeAlias { name, alias }) => {
+				v.visit_ident(name);
+				if let Some(ty) = alias {
+					v.visit_ty(ty);
+				}
+			}
+			ItemKind::Struct {
+				name,
+				generics,
+				fields,
+			} => {
+				v.visit_ident(name);
+				v.visit_generics(generics);
+				v.visit_fields(fields);
+			}
+			ItemKind::Enum {
+				name,
+				generics,
+				variants,
+			} => {
+				v.visit_ident(name);
+				v.visit_generics(generics);
+				v.visit_variants(variants);
+			}
+			ItemKind::Trait {
+				name,
+				generics,
+				members,
+			} => {
+				v.visit_ident(name);
+				v.visit_generics(generics);
+				v.visit_items(members);
+			}
+			ItemKind::TraitImpl {
+				type_,
+				trait_,
+				members,
+			} => {
+				v.visit_path(type_);
+				v.visit_path(trait_);
+				v.visit_items(members);
+			}
+			ItemKind::ForeignMod { items } => {
+				v.visit_items(items);
+			}
+		}
+	}
+
+	pub fn visit_expr<V: Visitor>(
+		v: &mut V,
+		Expr {
+			kind,
+			span: _,
+			id: _,
+		}: &Expr,
+	) {
+		match kind {
+			ExprKind::Access { path } => v.visit_path(path),
+			ExprKind::LiteralStr { sym: _ }
+			| ExprKind::LiteralInt { sym: _ }
+			| ExprKind::LiteralFloat { sym: _ } => {}
+			ExprKind::Paren { expr } => v.visit_expr(expr),
+			ExprKind::Unary { op: _, expr } => v.visit_expr(expr),
+			ExprKind::Binary { op: _, left, right } => {
+				v.visit_expr(left);
+				v.visit_expr(right);
+			}
+			ExprKind::ShortCircuit { op: _, left, right } => {
+				v.visit_expr(left);
+				v.visit_expr(right);
+			}
+			ExprKind::FnCall { expr, args } => {
+				v.visit_expr(expr);
+				for arg in &args.bit {
+					v.visit_expr(arg);
+				}
+			}
+			ExprKind::If {
+				cond,
+				conseq,
+				altern,
+			} => {
+				v.visit_expr(cond);
+				v.visit_block(conseq);
+				if let Some(altern) = altern {
+					v.visit_block(altern);
+				}
+			}
+			ExprKind::Match { expr, arms: _ } => {
+				v.visit_expr(expr);
+			}
+			ExprKind::WhileLoop { check, body } => {
+				v.visit_expr(check);
+				v.visit_block(body);
+			}
+			ExprKind::Loop { body } => v.visit_block(body),
+			ExprKind::Method { expr, name, params } => {
+				v.visit_expr(expr);
+				v.visit_ident(name);
+				for param in params {
+					v.visit_expr(param);
+				}
+			}
+			ExprKind::Field { expr, name } => {
+				v.visit_expr(expr);
+				v.visit_ident(name);
+			}
+			ExprKind::Deref { expr } => v.visit_expr(expr),
+			ExprKind::Assign { target, value } => {
+				v.visit_expr(target);
+				v.visit_expr(value);
+			}
+			ExprKind::Return { expr } => {
+				if let Some(expr) = expr {
+					v.visit_expr(expr);
+				}
+			}
+			ExprKind::Break { expr, label } => {
+				if let Some(expr) = expr {
+					v.visit_expr(expr);
+				}
+				if let Some(label) = label {
+					v.visit_ident(&label.bit);
+				}
+			}
+			ExprKind::Continue { label } => {
+				if let Some(label) = label {
+					v.visit_ident(&label.bit);
+				}
+			}
+		}
+	}
+
+	pub fn visit_block<V: Visitor>(
+		v: &mut V,
+		Block {
+			stmts,
+			span: _,
+			id: _,
+		}: &Block,
+	) {
+		v.visit_stmts(stmts);
+	}
+
+	pub fn visit_stmt<V: Visitor>(
+		v: &mut V,
+		Stmt {
+			kind,
+			span: _,
+			id: _,
+		}: &Stmt,
+	) {
+		match kind {
+			StmtKind::Let {
+				name,
+				ty,
+				value,
+				mutable: _,
+			} => {
+				v.visit_ident(name);
+				if let Some(ty) = ty {
+					v.visit_ty(ty);
+				}
+				if let Some(value) = value {
+					v.visit_expr(value);
+				}
+			}
+			StmtKind::Expr(expr) | StmtKind::ExprRet(expr) => v.visit_expr(expr),
+			StmtKind::Empty => {}
+		}
+	}
+
+	pub fn visit_ty<V: Visitor>(v: &mut V, Ty { kind, span: _ }: &Ty) {
+		match kind {
+			TyKind::Path(path) => v.visit_path(path),
+			TyKind::Pointer(ty) => v.visit_ty(ty),
+			TyKind::Unit => {}
+		}
+	}
+
+	pub fn visit_path<V: Visitor>(
+		v: &mut V,
+		Path {
+			segments,
+			span: _,
+			id: _,
+		}: &Path,
+	) {
+		for segment in segments {
+			v.visit_path_segment(segment);
+		}
+	}
+
+	pub fn visit_path_segment<V: Visitor>(
+		v: &mut V,
+		PathSegment {
+			name,
+			generics,
+			span: _,
+		}: &PathSegment,
+	) {
+		v.visit_ident(name);
+		v.visit_generic_params(generics);
+	}
+
+	pub fn visit_generic_params<V: Visitor>(
+		v: &mut V,
+		GenericParams { params, span: _ }: &GenericParams,
+	) {
+		for param in params {
+			v.visit_ty(param);
+		}
+	}
+
+	pub fn visit_param<V: Visitor>(v: &mut V, Param { name, ty, id: _ }: &Param) {
+		v.visit_ident(name);
+		v.visit_ty(ty);
+	}
+
+	pub fn visit_field_def<V: Visitor>(v: &mut V, FieldDef { name, ty, span: _ }: &FieldDef) {
+		v.visit_ident(name);
+		v.visit_ty(ty);
+	}
+
+	pub fn visit_variant<V: Visitor>(
+		v: &mut V,
+		Variant {
+			name,
+			kind,
+			span: _,
+		}: &Variant,
+	) {
+		v.visit_ident(name);
+		match kind {
+			VariantKind::Bare => {}
+			VariantKind::Tuple(tys) => {
+				for ty in tys {
+					v.visit_ty(ty);
+				}
+			}
+			VariantKind::Struct(fields) => {
+				v.visit_fields(fields);
+			}
+		}
+	}
+
+	pub fn visit_generics<V: Visitor>(v: &mut V, Generics { idents, span: _ }: &Generics) {
+		for generic in idents {
+			let Generic { name, id: _ } = generic;
+			v.visit_ident(name);
+		}
+	}
+
+	pub fn visit_ident<V: Visitor>(_v: &mut V, Ident { sym: _, span: _ }: &Ident) {}
 }
--- src/resolve.rs
+++ src/resolve.rs
@@ -3,34 +3,55 @@ use std::{collections::hash_map::Entry, fmt};
 use rustc_hash::FxHashMap;
 
 use crate::{
-	ast::{self, AttrMeta, ExprKind, Visitor},
+	ast::{self, AttrMeta, ExprKind, NodeId, visit},
 	errors, hir,
-	session::{DcxHandle, SessionCtx},
+	session::{DcxHandle, Diagnostic, SessionCtx},
 	symbols::{Symbol, sym},
 };
 
-pub(crate) fn collect_root(scx: &SessionCtx, ast: &ast::Root) -> NameEnvironment {
+pub(crate) fn collect_root(scx: &SessionCtx, ast: &ast::Root) -> CollectionResult {
 	let mut collector = Collector::new(scx);
-	collector.visit_root(ast);
+	visit::Visitor::visit_root(&mut collector, ast);
 
-	dbg!(collector.lang_items);
-	collector.name_env
+	let Collector {
+		name_env,
+		lang_items,
+		..
+	} = collector;
+	CollectionResult {
+		name_env,
+		lang_items,
+	}
 }
 
-pub(crate) fn resolve_root(scx: &SessionCtx, ast: &ast::Root) {
-	let mut resolver = Resolver::new(scx);
-	resolver.visit_root(ast);
+pub struct CollectionResult {
+	pub name_env: NameEnvironment,
+	pub lang_items: FxHashMap<LangItemKind, DefId>,
+}
 
-	todo!()
+pub(crate) fn resolve_root(
+	scx: &SessionCtx,
+	ast: &ast::Root,
+	name_env: &NameEnvironment,
+) -> ResolutionResult {
+	let mut resolver = Resolver::new(scx, name_env);
+	visit::Visitor::visit_root(&mut resolver, ast);
+
+	let Resolver { resolution_map, .. } = resolver;
+	ResolutionResult { resolution_map }
+}
+
+pub struct ResolutionResult {
+	pub resolution_map: FxHashMap<ast::NodeId, Resolution>,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
-pub struct LocalDefId(pub u32);
+pub struct DefId(u32);
 
-impl fmt::Debug for LocalDefId {
+impl fmt::Debug for DefId {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		// local def id -> did
-		// def id -> did{<module>}
+		// def id -> did#x
+		// TODO: global def id -> did{<package id>}#x
 		write!(f, "did#{}", self.0)
 	}
 }
@@ -41,13 +62,14 @@ pub enum LangItemKind {
 	AddAssignTrait,
 	SubTrait,
 	SubAssignTrait,
-
+	//
+	// MulTrait,
+	// DivTrait,
+	//
 	BoolTy,
 	UIntTy,
 	SIntTy,
 	FloatTy,
-	// MulTrait,
-	// DivTrait,
 	// and much more :)
 }
 
@@ -67,7 +89,7 @@ impl LangItemKind {
 	}
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Namespace {
 	Type,
 	Value,
@@ -82,17 +104,20 @@ impl fmt::Display for Namespace {
 	}
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone, Default)]
 pub struct NameEnvironment {
-	pub types: FxHashMap<Symbol, ast::NodeId>,
-	pub values: FxHashMap<Symbol, ast::NodeId>,
+	pub types: FxHashMap<Symbol, DefId>,
+	pub values: FxHashMap<Symbol, DefId>,
 }
 
 #[derive(Debug)]
 struct Collector<'scx> {
 	scx: &'scx SessionCtx,
 	pub(crate) name_env: NameEnvironment,
-	pub(crate) lang_items: FxHashMap<LangItemKind, LocalDefId>,
+	pub(crate) lang_items: FxHashMap<LangItemKind, DefId>,
+
+	next_local_def_id: u32,
+	node_id_to_def_id: FxHashMap<ast::NodeId, DefId>,
 }
 
 impl<'scx> Collector<'scx> {
@@ -102,51 +127,63 @@ impl<'scx> Collector<'scx> {
 			scx,
 			name_env: NameEnvironment::default(),
 			lang_items: FxHashMap::default(),
+			next_local_def_id: 0,
+			node_id_to_def_id: Default::default(),
 		}
 	}
 }
 
 impl Collector<'_> {
-	fn register_lang_item(&mut self, kind: LangItemKind, item: &ast::Item) {
-		let local_def_id = todo!("local def id from item {:?}", item.id);
-		let before = self.lang_items.insert(kind, local_def_id);
+	fn create_def(&mut self, ast_id: NodeId) -> DefId {
+		let def_id = DefId(self.next_local_def_id);
+		self.next_local_def_id += 1;
+		self.node_id_to_def_id.insert(ast_id, def_id);
+		def_id
+	}
+
+	fn register_lang_item(&mut self, kind: LangItemKind, def_id: DefId) {
+		let before = self.lang_items.insert(kind, def_id);
 		assert!(before.is_none());
 	}
 
-	fn register_def(&mut self, ns: &Namespace, item: &ast::Item, name: &ast::Ident) {
+	fn register_def(&mut self, ns: Namespace, def_id: DefId, name: &ast::Ident) {
 		let map = match ns {
 			Namespace::Type => &mut self.name_env.types,
 			Namespace::Value => &mut self.name_env.values,
 		};
 
 		match map.entry(name.sym) {
-			Entry::Vacant(vacant) => _ = vacant.insert(item.id),
+			Entry::Vacant(vacant) => _ = vacant.insert(def_id),
 			Entry::Occupied(occupied) => {
-				let item_id = occupied.get();
-				let report = errors::ty::item_name_conflict(todo!("{item_id:?}"), item.span, ns);
+				let (span1, span2) = todo!("get spans for {:?} and {:?}", occupied.get(), def_id);
+				let report = errors::ty::item_name_conflict(span1, span2, ns);
 				self.scx.dcx().emit_build(report);
 			}
 		}
 	}
+
+	fn parse_lang_item(
+		&mut self,
+		item: &ast::Item,
+		attr: &ast::Attr,
+	) -> Result<LangItemKind, Diagnostic> {
+		let AttrMeta::Tuple(exprs) = &attr.meta else {
+			todo!("wrong syntax for `lang_item` attr");
+		};
+		let &[expr] = &exprs.as_slice() else {
+			todo!("wrong syntax")
+		};
+		let ExprKind::LiteralStr { sym } = expr.kind else {
+			todo!("wrong syntax")
+		};
+		let Some(kind) = LangItemKind::parse(sym) else {
+			todo!("this lang item doesn't exist")
+		};
+		Ok(kind)
+	}
 }
 
-impl ast::Visitor for Collector<'_> {
-	fn visit_root(&mut self, ast::Root { attrs, items }: &ast::Root) {
-		self.visit_attrs(attrs);
-		self.visit_items(items);
-	}
-
-	fn visit_attr(
-		&mut self,
-		ast::Attr {
-			path,
-			meta,
-			span,
-			id,
-		}: &ast::Attr,
-	) {
-	}
-
+impl visit::Visitor for Collector<'_> {
 	fn visit_item(
 		&mut self,
 		item @ ast::Item {
@@ -156,40 +193,30 @@ impl ast::Visitor for Collector<'_> {
 			id,
 		}: &ast::Item,
 	) {
+		let def_id = self.create_def(*id);
+
 		for attr in attrs {
 			if attr.path.is_match(&[sym::lang_item]) {
-				let AttrMeta::Tuple(exprs) = &attr.meta else {
-					todo!("wrong syntax for `lang_item` attr");
-				};
-				let &[expr] = &exprs.as_slice() else {
-					todo!("wrong syntax")
-				};
-				let ExprKind::LiteralStr { sym } = expr.kind else {
-					todo!("wrong syntax")
-				};
-				let Some(kind) = LangItemKind::parse(sym) else {
-					todo!("this lang item doesn't exist")
-				};
-				self.register_lang_item(kind, item);
+				match self.parse_lang_item(item, attr) {
+					Ok(kind) => self.register_lang_item(kind, def_id),
+					Err(diag) => self.scx.dcx().emit(&diag),
+				}
 			}
 		}
 
 		match kind {
 			ast::ItemKind::Struct { name, .. }
 			| ast::ItemKind::Enum { name, .. }
-			| ast::ItemKind::TypeAlias(ast::TypeAlias { name, .. }) => {
-				self.register_def(&Namespace::Type, item, name);
-			}
-			ast::ItemKind::Trait { name, .. } => {
-				self.register_def(&Namespace::Type, item, name);
-				// TODO: register trait in a separate category
+			| ast::ItemKind::TypeAlias(ast::TypeAlias { name, .. })
+			| ast::ItemKind::Trait { name, .. } => {
+				self.register_def(Namespace::Type, def_id, name);
 			}
 
 			ast::ItemKind::Function(ast::Function { name, .. }) => {
-				self.register_def(&Namespace::Value, item, name);
+				self.register_def(Namespace::Value, def_id, name);
 			}
-			ast::ItemKind::ForeignMod { items } => {
-				self.visit_items(items);
+			ast::ItemKind::ForeignMod { .. } => {
+				ast::visit::visit_item(self, item);
 			}
 
 			ast::ItemKind::TraitImpl { .. } => {
@@ -201,23 +228,22 @@ impl ast::Visitor for Collector<'_> {
 }
 
 #[derive(Debug, Clone)]
-pub enum Resolved {
-	// Def(resolve::DefId),
-	Def(hir::ItemId),
-	Local(hir::ExprId),
+pub enum Resolution {
+	Def(DefId),
+	Local(ast::NodeId),
 }
 
-impl Resolved {
-	pub fn as_def(&self) -> Option<hir::ItemId> {
+impl Resolution {
+	pub fn as_def(&self) -> Option<DefId> {
 		match self {
-			Self::Def(def) => Some(*def),
+			Self::Def(def) => todo!(),
 			_ => None,
 		}
 	}
 
 	pub fn as_local(&self) -> Option<hir::ExprId> {
 		match self {
-			Self::Local(id) => Some(*id),
+			Self::Local(id) => todo!(),
 			_ => None,
 		}
 	}
@@ -226,31 +252,151 @@ impl Resolved {
 #[derive(Debug, Clone)]
 struct Resolver<'scx> {
 	scx: &'scx SessionCtx,
+
+	name_env: &'scx NameEnvironment,
+
+	value_layers: Vec<(ValueLayerKind, FxHashMap<Symbol, ast::NodeId>)>,
+	type_layers: Vec<(TypeLayerKind, FxHashMap<Symbol, ast::NodeId>)>,
+	resolution_map: FxHashMap<ast::NodeId, Resolution>,
 }
 
 impl<'scx> Resolver<'scx> {
 	#[must_use]
-	pub fn new(scx: &'scx SessionCtx) -> Self {
-		Self { scx }
+	pub fn new(scx: &'scx SessionCtx, name_env: &'scx NameEnvironment) -> Self {
+		Self {
+			scx,
+			name_env,
+			value_layers: Vec::default(),
+			type_layers: Vec::default(),
+			resolution_map: FxHashMap::default(),
+		}
 	}
 }
 
-impl Visitor for Resolver<'_> {
-	fn visit_root(&mut self, ast::Root { attrs, items }: &ast::Root) {
-		self.visit_attrs(attrs);
-		self.visit_items(items);
+impl Resolver<'_> {
+	fn with_value_bindings(
+		&mut self,
+		layer_kind: ValueLayerKind,
+		bindings: FxHashMap<Symbol, ast::NodeId>,
+		f: impl FnOnce(&mut Self),
+	) {
+		self.value_layers.push((layer_kind, bindings));
+		f(self);
+		self.value_layers.pop();
 	}
 
-	fn visit_attr(&mut self, attrs: &ast::Attr) {}
-
-	fn visit_item(
+	fn with_type_bindings(
 		&mut self,
-		ast::Item {
-			attrs,
-			kind,
-			span,
-			id,
-		}: &ast::Item,
+		layer_kind: TypeLayerKind,
+		bindings: FxHashMap<Symbol, ast::NodeId>,
+		f: impl FnOnce(&mut Self),
 	) {
+		self.type_layers.push((layer_kind, bindings));
+		f(self);
+		self.type_layers.pop();
+	}
+}
+
+#[derive(Debug, Clone)]
+pub enum ValueLayerKind {
+	Param,
+	Local,
+}
+
+#[derive(Debug, Clone)]
+pub enum TypeLayerKind {
+	Generics,
+}
+
+impl Resolver<'_> {
+	fn resolve_path(&mut self, ns: &Namespace, path: &ast::Path) {
+		let path_simple = path.simple();
+
+		let res = match ns {
+			Namespace::Type => 'res: {
+				for (layer_kind, bindings) in self.type_layers.iter().rev() {
+					if let Some(id) = bindings.get(&path_simple.sym) {
+						break 'res match layer_kind {
+							TypeLayerKind::Generics => Resolution::Local(*id),
+						};
+					}
+				}
+				let Some(node_id) = self.name_env.types.get(&path_simple.sym) else {
+					todo!("could not find type in scope: {path_simple:?}")
+				};
+				Resolution::Def(*node_id)
+			}
+			Namespace::Value => 'res: {
+				for (layer_kind, bindings) in self.value_layers.iter().rev() {
+					if let Some(id) = bindings.get(&path_simple.sym) {
+						break 'res match layer_kind {
+							ValueLayerKind::Param => Resolution::Local(*id),
+							ValueLayerKind::Local => Resolution::Local(*id),
+						};
+					}
+				}
+				let Some(node_id) = self.name_env.values.get(&path_simple.sym) else {
+					todo!("could not find value in scope: {path_simple:?}")
+				};
+				Resolution::Def(*node_id)
+			}
+		};
+
+		let before = self.resolution_map.insert(path.id, res);
+		assert!(before.is_none());
+	}
+}
+
+impl visit::Visitor for Resolver<'_> {
+	fn visit_item(&mut self, item: &ast::Item) {
+		match &item.kind {
+			ast::ItemKind::Function(ast::Function { decl, body, .. }) => {
+				self.with_value_bindings(
+					ValueLayerKind::Param,
+					decl.params.iter().map(|p| (p.name.sym, p.id)).collect(),
+					|this| {
+						for param in &decl.params {
+							this.visit_param(param);
+						}
+						if let Some(ret) = &decl.ret {
+							this.visit_ty(ret);
+						}
+						if let Some(body) = body {
+							this.visit_block(body);
+						}
+					},
+				);
+			}
+			ast::ItemKind::Struct { generics, .. }
+			| ast::ItemKind::Enum { generics, .. }
+			| ast::ItemKind::Trait { generics, .. } => self.with_type_bindings(
+				TypeLayerKind::Generics,
+				generics.idents.iter().map(|g| (g.name.sym, g.id)).collect(),
+				|this| ast::visit::visit_item(this, item),
+			),
+			_ => ast::visit::visit_item(self, item),
+		}
+	}
+
+	fn visit_ty(&mut self, ty: &ast::Ty) {
+		if let ast::TyKind::Path(path) = &ty.kind {
+			self.resolve_path(&Namespace::Type, path);
+		}
+		ast::visit::visit_ty(self, ty);
+	}
+
+	fn visit_stmt(&mut self, stmt: &ast::Stmt) {
+		if let ast::StmtKind::Let { name, .. } = &stmt.kind {
+			let (_layer_kind, bindings) = self.value_layers.last_mut().unwrap();
+			bindings.insert(name.sym, stmt.id);
+		}
+		ast::visit::visit_stmt(self, stmt);
+	}
+
+	fn visit_expr(&mut self, expr: &ast::Expr) {
+		if let ast::ExprKind::Access { path } = &expr.kind {
+			self.resolve_path(&Namespace::Value, path);
+		}
+		ast::visit::visit_expr(self, expr);
 	}
 }
